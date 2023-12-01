f9a794e394100fb0de9cd4727ee44096cd09ba0b
Emoncr <biplobemon 75945@gmail.com>
2023-12-02 02:58:45 +0600

    created hero section and other pages

--- src/components/SiteNavbar.jsx
+++ src/components/SiteNavbar.jsx
@@ -4,78 +4,75 @@ const SiteNavbar = () => {
 
 
     return (
-        <div>
-            <div className="container mx-auto">
-                <nav className="flex items-center justify-between py-9 relative">
-                    <div className="logo_container">
-                        <img src="/images/DesignAGENCY.png" alt="logo" />
-                    </div>
-                    <div className="nav_items">
-                        <ul className="flex items-center gap-[30px] ">
-                            <li>
-                                <Link
-                                    href={"/"}
-                                    className="p-[10px] text-center text-black font-poppins font-medium capitalize "
-                                >
-                                    Home
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    href={"/"}
-                                    className="p-[10px] text-center text-black font-poppins font-medium capitalize "
-                                >
-                                    Team
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    href={"/"}
-                                    className="p-[10px] text-center text-black font-poppins font-medium capitalize "
-                                >
-                                    Service
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    href={"/"}
-                                    className="p-[10px] text-center text-black font-poppins font-medium capitalize "
-                                >
-                                    Projects
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    href={"/"}
-                                    className="p-[10px] text-center text-black font-poppins font-medium capitalize "
-                                >
-                                    Testimonials
-                                </Link>
-                            </li>
+        <div className="container mx-auto absolute   transform left-1/2 -translate-x-1/2 ">
+            <nav className="flex items-center justify-between py-9 relative">
+                <div className="logo_container">
+                    <img src="/images/logo.png" alt="logo" />
+                </div>
+                <div className="nav_items">
+                    <ul className="flex items-center gap-[30px] ">
+                        <li>
+                            <Link
+                                href={"/"}
+                                className="p-[10px] text-center text-black font-poppins font-medium capitalize "
+                            >
+                                Home
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/"}
+                                className="p-[10px] text-center text-black font-poppins font-medium capitalize "
+                            >
+                                Team
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/"}
+                                className="p-[10px] text-center text-black font-poppins font-medium capitalize "
+                            >
+                                Service
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/"}
+                                className="p-[10px] text-center text-black font-poppins font-medium capitalize "
+                            >
+                                Projects
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                href={"/"}
+                                className="p-[10px] text-center text-black font-poppins font-medium capitalize "
+                            >
+                                Testimonials
+                            </Link>
+                        </li>
 
-                            <li>
-                                <Link
-                                    href={"/"}
-                                    className="font-poppins  font-medium text-black py-[10px] px-6 border-brandGreen border rounded-lg hover:border-transparent hover:bg-brandGreen duration-300 hover:text-white"
-                                >
-                                    Login
-                                </Link>
-                            </li>
-                            <li>
-                                <Link
-                                    className="font-poppins  font-medium text-white py-[10px] px-6 bg-brandGreen border border-transparent rounded-lg hover:bg-transparent hover:border-brandGreen duration-300 hover:text-black "
-                                    href={"/"}
-                                >
+                        <li>
+                            <Link
+                                href={"/"}
+                                className="font-poppins  font-medium text-black py-[10px] px-6 border-brandGreen border rounded-lg hover:border-transparent hover:bg-brandGreen duration-300 hover:text-white"
+                            >
+                                Login
+                            </Link>
+                        </li>
+                        <li>
+                            <Link
+                                className="font-poppins  font-medium text-white py-[10px] px-6 bg-brandGreen border border-transparent rounded-lg hover:bg-transparent hover:border-brandGreen duration-300 hover:text-black "
+                                href={"/"}
+                            >
 
-                                    Register
-                                </Link>
-                            </li>
+                                Register
+                            </Link>
+                        </li>
 
-                        </ul>
-                    </div>
-                </nav>
-            </div>
-
+                    </ul>
+                </div>
+            </nav>
         </div>
     );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -23,7 +23,7 @@ module.exports = {
       boxShadow: {
         teamShadow: "0px 4.4px 20px -1px rgba(19, 16, 34, 0.05)",
         brandShadow: "0px 4px 30px 0px rgba(0, 0, 0, 0.09)",
-      }
+      },
     },
   },
   plugins: [],
