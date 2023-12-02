c8dc2134ccbcc14b0da05ed147ef4b061e39d32d
Emoncr <biplobemon 75945@gmail.com>
2023-12-02 18:50:37 +0600

    testimonial ui complate

--- src/app/testimonials/page.jsx
+++ src/app/testimonials/page.jsx
@@ -1,8 +1,13 @@
+import Subscribe from '@/components/Subscribe'
+import TestimonialList from '@/components/TestimonialList'
 import React from 'react'
 
 const Testimonial = () => {
     return (
-        <div>Testimonial</div>
+        <>
+            <TestimonialList />
+            <Subscribe />
+        </>
     )
 }
 
